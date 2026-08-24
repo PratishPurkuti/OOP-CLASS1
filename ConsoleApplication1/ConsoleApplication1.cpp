9530202e98ff4b29ef2a83104e93847f1b42c31a
ppurkuti0506 <ppurkuti0506@BO103-LAB-52050>
2026-08-24 11:32:52 -0500

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Stack {
+    int* stack;
+    void push(int x) {
+
+    }
+    int pop() {
+
+    }
+};
+
+int main(int argc, char** argv) {
+    cout << "Hello World!" << endl;
+    Stack stack;
+}
